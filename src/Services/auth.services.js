4b4772001559ef70e021e2e767a01d389cbd1500
Lorgio Roda <lorgiorodaroca@gmail.com>
2021-05-14 17:23:38 -0400

    route home 🚧

---
+++ src/Services/auth.services.js
@@ -0,0 +1,11 @@
+import axios from "axios";
+
+export default class aut extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
---
+++ src/pages/Home/Home.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import SCHome from './Home.styled';
+
+function Home() {
+    return (
+        <div>
+            <div className="home">
+                <h1>Bullet Journal</h1>
+                <p>Description</p>
+                <div className="home__bottom">
+                    
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Home;
