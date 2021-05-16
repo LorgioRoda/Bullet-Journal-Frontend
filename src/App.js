37dcc3f168811004a919f29e5fadf0b5b09ddab0
Lorgio Roda <lorgiorodaroca@gmail.com>
2021-05-16 18:18:53 -0400

    signup working🚧

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import logo from "./logo.svg";
 import "./App.css";
 import React, { Component }  from 'react';
 import { Route, Switch } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import Task from "./pages/Task/task";
+import Signup from "./pages/Signup/Signup";
 
 function App() {
   return (
@@ -11,6 +11,7 @@ function App() {
       <Switch>
         <Route exact path="/" component={Home} />
         <Route exact path="/task" component={Task} />
+        <Route exact path="/signup" component={Signup}/>
       </Switch>
     </div>
   );
