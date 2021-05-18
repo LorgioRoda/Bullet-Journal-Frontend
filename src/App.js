c7644768dd38eb7dd680ef71fc8c5880320c2b2f
Adrian <adrian.asencio92@hotmail.com>
2021-05-18 12:43:57 +0200

    actualizando

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import "./App.css";
-import React, { Component }  from 'react';
+import React, { Component } from "react";
 import { Route, Switch } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import Task from "./pages/Task/task";
 import Signup from "./pages/Signup/Signup";
 import PrivateRoute from "./components/PrivateRoute/PrivateRoute";
-import AnonRoute from "./components/AnonRoute/AnonRoute"
-import Login from "./pages/Login/Login"
-import Dashboard from "./pages/Dashboard/Dashboard"
-
+import AnonRoute from "./components/AnonRoute/AnonRoute";
+import Login from "./pages/Login/Login";
+import Dashboard from "./pages/Dashboard/Dashboard";
 
 function App() {
   return (
@@ -16,9 +15,10 @@ function App() {
       <Dashboard />
       <Switch>
         <Route exact path="/" component={Home} />
-        <PrivateRoute path="/task" exact component={Task} /> {/* Path and Exact props */}
-        <AnonRoute exact path="/signup" component={Signup} redirectPath="/"/>
-        <AnonRoute exact path="/login" component={Login}/>
+        <PrivateRoute path="/task" exact component={Task} />{" "}
+        {/* Path and Exact props */}
+        <AnonRoute exact path="/signup" component={Signup} redirectPath="/" />
+        <AnonRoute exact path="/login" component={Login} />
       </Switch>
     </div>
   );
--- src/pages/Login/Login.styled.js
+++ src/pages/Login/Login.styled.js
@@ -31,8 +31,6 @@ const SCLogin = styled.section`
 
   .form{
       margin: 30px;
-      
-      
   }
 
   .form-item{ 
@@ -40,14 +38,11 @@ const SCLogin = styled.section`
     display: flex;
     flex-direction: column;
     margin-bottom: 10px;
-    
-    
   }
 
   .label{
       margin-bottom: 10px;
       font-size: 20px;
-      
   }
 
   .input{
@@ -59,36 +54,31 @@ const SCLogin = styled.section`
       outline: none;
       cursor: pointer;
       border: 1px solid rgb(0,110,147);
-      
   }
 
   .input:hover{
     box-shadow: rgba(17, 17, 26, 0.05) 0px 1px 0px, rgba(17, 17, 26, 0.1) 0px 0px 8px;
-    
   }
 
   .hr{
     margin-top: 25px;
        background-color: rgb(0,110,147);
        height: 3px;
-       
   }
+
   .p-hr{
        margin-top: 25px;
        font-size:12px;
        color: rgb(0,110,147);
-       
   }
 
   .boton{
       margin-top:10px;
-      
   }
 
   .boton2{
       margin-top:10px;
       background-color: green;
-      
   }
 
   .boton2:hover{
